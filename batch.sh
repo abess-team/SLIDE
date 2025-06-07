048c5fc9d33dd8eba3e7c12eaeb13eb793bb9d81
Mamba413 <J.Zhu69@lse.ac.uk>
2025-06-08 00:05:56 +0800

    batch script

---
+++ batch.sh
@@ -0,0 +1,11 @@
+# Rscript simu_ws.R --type=8 --method="nodewise_logistic_gic2"
+# Rscript simu_ws.R --type=10 --method="nodewise_logistic_gic2"
+
+# Rscript simu_p.R --type=8 --method="nodewise_logistic_gic2"
+# Rscript simu_p.R --type=8 --method="RPLE_thres"
+# Rscript simu_p.R --type=8 --method="logRISE_thres"
+# Rscript simu_p.R --type=8 --method="RISE_thres"
+
+Rscript simu_p.R --type=10 --method="nodewise_logistic_gic2"
+
+/usr/bin/shutdown 
