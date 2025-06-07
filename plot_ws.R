7352471db307b01a94c01cd67a06ed08d4317f29
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2025-06-07 15:58:15 +0100

    update results

--- plot_ws.R
+++ plot_ws.R
@@ -2,12 +2,12 @@
 rm(list = ls()); gc(reset = TRUE)
 library(reshape2)
 library(ggplot2)
-# library(ggpmisc)
+library(ggpmisc)
 
 # path <- "D:\\ising-L0"
 # path <- "~/splicing-ising/code-simulate/0516"
 # path <- "/Users/zhujin/splicing-ising/code-simulate/0910/result_p"
-path <- "C:/Users/ZHUJ68/SLIDE/"
+path <- "C:/Users/ZHUJ68/SLIDE/result_ws/"
 setwd(path)
 
 # method_list <- c("RPLE_thres", "nodewise_logistic_gic2")
@@ -15,34 +15,31 @@ setwd(path)
 # method_list <- c("RPLE_thres", "logRISE_thres")
 method_list <- c("nodewise_logistic_gic2")
 
-# case_list <- 2
+p_list <- c(16)
+# case_list <- 1
 case_list <- c(1, 2)
 res <- list()
 for (case in case_list) {
   if (case == 1) {
-    type <- 10
-
-    degree_list <- 4
-    
-    alpha_list <- 0.3
-    beta_list <- 0.4
-    p_list <- c(9, 16, 25, 36, 49, 64, 81, 100)
-  } else {
-    type <- 8
-    
+    omega <- 2.0
     degree_list <- c(3)
-    
-    alpha_list <- 0.3
-    beta_list <- 0.6
-    
-    p_list <- c(8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52)
+    alpha_list <- seq(30, 9, length=8) / 50
+    beta_list <- (omega - alpha_list) / (degree_list - 1)
+    type <- 8
+  } else {
+    omega <- 0.9
+    degree_list <- c(4)
+    alpha_list <-3 * (seq(30, 3, length=10) / 400)
+    beta_list <- (omega - alpha_list) / (degree_list - 1)
+    type <- 10
   }
   
-  n_matrix <- matrix(0, length(method_list), length(p_list))
+  n_matrix <- matrix(0, length(method_list), length(alpha_list))
   for(i in 1: length(method_list)) {
-    for(j in 1:length(p_list)) {
-      name <- paste0("result_p/", method_list[i], "_", "type", type, 
-                     "_p", p_list[j], "_alpha", alpha_list, "_beta", beta_list, 
+    for(j in 1:length(alpha_list)) {
+      name <- paste0(method_list[i], "_", 
+                     "type", type, "_p", p_list, 
+                     "_alpha", alpha_list[j], "_beta", beta_list[j], 
                      "_degree", degree_list, "_test.csv")
       
       tmp <- read.csv(name)
@@ -57,7 +54,8 @@ for (case in case_list) {
     }
   }
   
-  res[[as.character(case)]] <- data.frame(t(n_matrix), "p" = p_list, "type" = type)
+  res[[as.character(case)]] <- data.frame(t(n_matrix), 
+                                          "alpha" = alpha_list, "type" = type)
 }
 pdat <- do.call("rbind.data.frame", res)
 
@@ -68,18 +66,18 @@ method_name[method_name == "nodewise"] <- "SLIDE"
 
 if (length(case_list) > 1) {
   colnames(pdat)[1:length(method_name)] <- method_name
-  data_melt <- melt(pdat, id.vars = c("type", "p"), 
+  data_melt <- melt(pdat, id.vars = c("type", "alpha"), 
                     variable.name = "Method", value.name = "n")
   data_melt[["type"]] <- ifelse(data_melt[["type"]] == 8, 
                                 "Random regular graph", 
                                 "Square lattice")
   
-  p <- ggplot(data = data_melt, aes(x = p, y = n, group = Method)) + 
+  p <- ggplot(data = data_melt, aes(x = 1 / alpha, y = n, group = Method)) + 
     facet_wrap(type ~ ., ncol = 2, scales = "free") + 
     geom_point(aes(color = Method, shape = Method), size = 2)+
     geom_smooth(aes(color = Method), se = FALSE, 
-                formula = y ~ log(x), method = "lm") + 
-    xlab(expression(italic(p)))+
+                formula = y ~ I(x^2) + x, method = "lm") + 
+    xlab(expression(italic(alpha)))+
     ylab("Optimal sample size")+
     scale_color_discrete(name = "Methods") +
     scale_shape_discrete(name = "Methods") + 
