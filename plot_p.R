7adbb72b0958bc39f085020b7d022c2eec11dbec
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2025-06-12 11:28:56 +0800

    Update plot_p.R

--- plot_p.R
+++ plot_p.R
@@ -2,18 +2,18 @@
 rm(list = ls()); gc(reset = TRUE)
 library(reshape2)
 library(ggplot2)
-# library(ggpmisc)
+library(ggpmisc)
 
 # path <- "D:\\ising-L0"
 # path <- "~/splicing-ising/code-simulate/0516"
-# path <- "/Users/zhujin/splicing-ising/code-simulate/0910/result_p"
-path <- "C:/Users/ZHUJ68/SLIDE/"
+path <- "/Users/zhujin/splicing-ising/code-simulate/0910"
+# path <- "C:/Users/ZHUJ68/SLIDE/"
 setwd(path)
 
 # method_list <- c("RPLE_thres", "nodewise_logistic_gic2")
 # method_list <- c("RPLE_thres")
 # method_list <- c("RPLE_thres", "logRISE_thres")
-method_list <- c("nodewise_logistic_gic2")
+method_list <- c("RPLE_thres", "RISE_thres", "logRISE_thres", "nodewise_logistic_gic2")
 
 # case_list <- 2
 case_list <- c(1, 2)
@@ -74,11 +74,16 @@ if (length(case_list) > 1) {
                                 "Random regular graph", 
                                 "Square lattice")
   
-  p <- ggplot(data = data_melt, aes(x = p, y = n, group = Method)) + 
+  formula <- y ~ log(x)
+  pj <- position_jitter(width = 1, height = 0)
+  p <- ggplot(data = subset(data_melt), aes(x = p, y = n, group = Method)) + 
     facet_wrap(type ~ ., ncol = 2, scales = "free") + 
-    geom_point(aes(color = Method, shape = Method), size = 2)+
-    geom_smooth(aes(color = Method), se = FALSE, 
-                formula = y ~ log(x), method = "lm") + 
+    geom_point(aes(color = Method, shape = Method), size = 2, position = pj) +
+    # geom_smooth(aes(color = Method), se = FALSE, 
+    #             formula = formula, method = "lm") +
+    stat_poly_line(aes(color = Method), formula = formula, se = FALSE) +
+    stat_poly_eq(aes(color = Method, label = after_stat(eq.label)),
+                 formula = formula) +
     xlab(expression(italic(p)))+
     ylab("Optimal sample size")+
     scale_color_discrete(name = "Methods") +
