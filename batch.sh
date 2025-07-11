23725fa32f4bfff91fff4b024fbf3a44bf92568d
Mamba413 <J.Zhu69@lse.ac.uk>
2025-07-11 15:48:20 +0800

    update batch script

--- batch.sh
+++ batch.sh
@@ -1,26 +1,25 @@
-# Rscript simu_ws.R --type=8 --method="nodewise_logistic_gic2"
+Rscript simu_ws.R --type=8 --method="nodewise_logistic_gic2"
 Rscript simu_ws.R --type=8 --method="RPLE_thres"
 Rscript simu_ws.R --type=8 --method="RISE_thres"
 Rscript simu_ws.R --type=8 --method="logRISE_thres"
 Rscript simu_ws.R --type=8 --method="ELASSO_thres"
 
-# Rscript simu_ws.R --type=10 --method="nodewise_logistic_gic2"
+Rscript simu_ws.R --type=10 --method="nodewise_logistic_gic2"
 Rscript simu_ws.R --type=10 --method="RPLE_thres"
 Rscript simu_ws.R --type=10 --method="RISE_thres"
 Rscript simu_ws.R --type=10 --method="logRISE_thres"
 Rscript simu_ws.R --type=10 --method="ELASSO_thres"
 
-# Rscript simu_p.R --type=8 --method="nodewise_logistic_gic2"
+Rscript simu_p.R --type=8 --method="nodewise_logistic_gic2"
 Rscript simu_p.R --type=8 --method="RPLE_thres"
 Rscript simu_p.R --type=8 --method="logRISE_thres"
 Rscript simu_p.R --type=8 --method="RISE_thres"
+Rscript simu_p.R --type=8 --method="ELASSO_thres"
 
-# Rscript simu_p.R --type=10 --method="nodewise_logistic_gic2"
+Rscript simu_p.R --type=10 --method="nodewise_logistic_gic2"
 Rscript simu_p.R --type=10 --method="RPLE_thres"
 Rscript simu_p.R --type=10 --method="logRISE_thres"
 Rscript simu_p.R --type=10 --method="RISE_thres"
-
-# Rscript simu_p.R --type=8 --method="ELASSO_thres"
-# Rscript simu_p.R --type=10 --method="ELASSO_thres"
+Rscript simu_p.R --type=10 --method="ELASSO_thres"
 
 /usr/bin/shutdown 
